3fa81b1c5c68c6e79c39062b51569a0bebbdf518
Davindra O'Neal <davindraoneal@gmail.com>
2019-02-04 20:25:07 -0400

    Add files via upload

---
+++ TestingInput.java
@@ -0,0 +1,23 @@
+package testinginput;
+
+/**
+ * 
+ * @author Davindra O'Neal
+ * Purpose of Program - To test and learn more about accepting inputs
+ */
+
+import java.util.Scanner; //Imports the scanner utility to create a new scanner to accept input
+public class TestingInput {
+    public static void main(String[] args) {
+      Scanner input = new Scanner(System.in);
+      System.out.println("What is your first name?");
+      String firstName = input.next(); // String are declared using "String"//When a scanner is accepting the input for a string no datatype needs to be specified.
+      System.out.println("What is your last name?");
+      String lastName = input.next();
+      System.out.println("You are " + firstName + " " + lastName );
+      
+      
+              
+    }
+    
+}
