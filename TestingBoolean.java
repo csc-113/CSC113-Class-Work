04c6a36fef5a90078ec9e2e6f27cc5e697de054f
Davindra O'Neal <davindraoneal@gmail.com>
2019-02-05 11:53:43 -0400

    Add files via upload

---
+++ TestingBoolean.java
@@ -0,0 +1,23 @@
+
+package testingboolean;
+
+/**
+ *
+ * @author princ
+ */
+import java.util.Scanner;
+public class TestingBoolean { // a boolean is a datatype that can only be either true or false
+    public static void main(String[] args) {
+        //The result of a comparison is a boolean value
+        //for example
+        double radius = 1;
+        System.out.println(radius > 0); // this will print "true" since the comparison is true
+        // here is something more interactive
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a number, if it is larger than 20 the computer will print true, if not it will print false!");
+        int number = input.nextInt();
+        System.out.println(number > 20);
+       
+    }
+    
+}
