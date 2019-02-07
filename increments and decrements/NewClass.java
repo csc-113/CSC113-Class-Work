31564727108398a813f7b1d1368d4bcbd39443d6
Banny544 <47332419+Banny544@users.noreply.github.com>
2019-02-07 18:02:35 -0400

    Add files via upload

---
+++ increments and decrements/NewClass.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package inclassassignment;
+
+/**
+ * class Csc 113 
+ * professor Colin A. Cousins
+ * @author Giovanni N. Wells 
+ * 
+ */
+public class NewClass {
+public static void main(String[] args)
+{
+int i = 5;
+int j = ++i;
+System.out.println(j);
+int b= 5;
+int c= b++;
+System.out.println(c);
+int y = 5;
+int u = --i;
+System.out.println(u);
+int t= 5;
+int r= t--;
+System.out.println(r);
+}
+}
