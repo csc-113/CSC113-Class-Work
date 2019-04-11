9857ffd5e853e5d267ae1c377c12d0152cdf02c6
Davindra O'Neal <davindraoneal@gmail.com>
2019-04-11 11:09:50 -0400

    Add files via upload

---
+++ SimpleCircle.java
@@ -0,0 +1,31 @@
+package inclassexercise;
+
+/**
+ * Class - CSC113
+ * @author Davindra O'Neal
+ * Purpose of Application - In class exercise
+ */
+public class SimpleCircle {
+    double radius;
+    
+    
+public SimpleCircle(){
+    
+}
+public SimpleCircle( double newRadius){
+    newRadius = radius;
+}
+
+public double getArea(){
+    return 0;
+}
+
+public double getPerimeter(){
+    return 0;
+}
+
+public void setRadius(double newRadius ){
+    radius = newRadius;
+}
+
+}
